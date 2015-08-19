b7d40254b8eba3a96946655b661be99b70a22c74
neil <neilwarner@gmail.com>
2015-08-19 17:02:58 -0500

    Refactor to use MoviePage class with more useful fields, etc.

--- app/src/main/java/com/neiljaywarner/yamoviesapp/model/YAMovie.java
+++ app/src/main/java/com/neiljaywarner/yamoviesapp/model/YAMovie.java
@@ -4,6 +4,22 @@ package com.neiljaywarner.yamoviesapp.model;
  * Created by neil on 8/19/15.
  */
 public class YAMovie {
-    public float vote_average;
-    public int vote_count;
+
+    public final String BASE_POSTER_PATH = "http://image.tmdb.org/t/p/";
+    public final String POSTER_PATH_SIZE_STRING = "w185";
+    private String original_title;
+    private String poster_path; //includes "/"
+
+
+    public String getPosterFullUrl() {
+        return BASE_POSTER_PATH + "/" + POSTER_PATH_SIZE_STRING + poster_path;
+    }
+
+    public String getOriginalTitle() {
+        return original_title;
+    }
+
+    //size is one of the following: "w92", "w154", "w185", "w342", "w500", "w780", or "original".
+    // For most phones we recommend using “w185”.
+    //so example URL is http://image.tmdb.org/t/p/w185/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg
 }
