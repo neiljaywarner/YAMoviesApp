715034157caaa8345e66cdbe83e3c857097f9372
neil <neilwarner@gmail.com>
2015-08-22 16:44:30 -0500

    Menu stubs.

--- app/src/main/java/com/neiljaywarner/yamoviesapp/MainActivity.java
+++ app/src/main/java/com/neiljaywarner/yamoviesapp/MainActivity.java
@@ -1,9 +1,10 @@
 package com.neiljaywarner.yamoviesapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,8 +29,12 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_sort_popularity) {
+            Toast.makeText(this.getApplicationContext(), "TODO: Sort by popularity.", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        if (id == R.id.action_sort_rating) {
             return true;
         }
 
