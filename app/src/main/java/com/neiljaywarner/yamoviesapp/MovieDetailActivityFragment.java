2f1316b457a8991ac5151f119dfcc39bdb017466
neil <neilwarner@gmail.com>
2015-09-10 23:08:42 -0500

    Add view for single dummy trailer and (oops, no atomic commit) starting code for custom view

--- app/src/main/java/com/neiljaywarner/yamoviesapp/MovieDetailActivityFragment.java
+++ app/src/main/java/com/neiljaywarner/yamoviesapp/MovieDetailActivityFragment.java
@@ -1,5 +1,7 @@
 package com.neiljaywarner.yamoviesapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,15 +10,21 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.neiljaywarner.yamoviesapp.model.RelatedVideo;
 import com.neiljaywarner.yamoviesapp.model.YAMovie;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class MovieDetailActivityFragment extends Fragment {
 
     TextView mTextViewTitle, mTextViewYear, mTextViewVoteAverage, mTextViewOverview;
 
+    @Bind(R.id.viewGroupTrailers)
+    ViewGroup viewGroupRelatedVideos;
     public MovieDetailActivityFragment() {
     }
 
@@ -25,6 +33,7 @@ public class MovieDetailActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         final View root = inflater.inflate(R.layout.fragment_movie_detail, container, false);
+        ButterKnife.bind(this, root);
 
         ImageView imageViewThumbnail = (ImageView) root.findViewById(R.id.imageViewThumbnail);
         mTextViewTitle = (TextView) root.findViewById(R.id.textViewTitle);
@@ -73,5 +82,31 @@ public class MovieDetailActivityFragment extends Fragment {
         mTextViewYear.setText(movie.getYear());
         mTextViewVoteAverage.setText(movie.getVoteAverage() + "/10");//TODO: Code cleanup here.
         mTextViewOverview.setText(movie.getOverview());
+
+        View viewTrailer = getTrailerView(RelatedVideo.getTestVideo()); //TODO: Custom View
+        viewGroupRelatedVideos.addView(viewTrailer);
     }
+
+    //TODO:  custom view with bound data like in dan lew tut. tried android data, didnt' work yet.
+    public View getTrailerView(final RelatedVideo relatedVideo) {
+        TextView tv = new TextView(this.getActivity());
+        tv.setText(relatedVideo.getName());
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInYoutube(relatedVideo);
+            }
+        });
+        return tv;
+    }
+
+    /// TODO: Consider moving this code to RelatedVideo.java
+
+    private void openInYoutube(RelatedVideo relatedVideo) {
+        //TODO: Try/catch or something to do when youtube is not on the app to open in the browser with key
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + relatedVideo.getKey()));
+        startActivity(i);
+    }
+
+
 }
---
+++ app/src/main/java/com/neiljaywarner/yamoviesapp/view/YoutubeThumbnailView.java
@@ -0,0 +1,41 @@
+package com.neiljaywarner.yamoviesapp.view;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.neiljaywarner.yamoviesapp.R;
+import com.neiljaywarner.yamoviesapp.model.RelatedVideo;
+import com.squareup.picasso.Picasso;
+
+/**
+ * RelatedView that has a special knowledge of how to bind relevant data.
+ * FROM https://github.com/dlew/android-custom-views-sample/blob/master/app/src/main/java/net/danlew/customviews/view/UserViewEncapsulated.java
+ * which is Apache Licensed by Dan Lew (appears to be). i saw his talk in 2014 in this.
+ */
+public class YoutubeThumbnailView extends LinearLayout {
+
+    private ImageView mIconView;
+    private TextView mNameView;
+
+    public YoutubeThumbnailView(Context context) {
+        super(context);
+
+        setOrientation(LinearLayout.VERTICAL);
+        setGravity(Gravity.CENTER);
+
+        LayoutInflater.from(context).inflate(R.layout.video_thumbnail_merge, this);
+        mIconView = (ImageView) findViewById(R.id.icon);
+        mNameView = (TextView) findViewById(R.id.name);
+    }
+
+    public void bind(RelatedVideo video) {
+
+        Picasso.with(mIconView.getContext()).load(video.getYoutubeThumbnailUrl()).into(mIconView);
+
+        mNameView.setText(video.getName());
+    }
+}
